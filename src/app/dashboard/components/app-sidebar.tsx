056ceefcc950a6ea16ed254510ed408479e5dd85
Mehmet <mehmetext68@gmail.com>
2025-05-19 21:19:24 +0300

    feat: add sidebar menu

--- src/app/dashboard/components/app-sidebar.tsx
+++ src/app/dashboard/components/app-sidebar.tsx
@@ -1,19 +1,35 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import {
   Sidebar,
   SidebarContent,
   SidebarHeader,
   SidebarRail,
 } from "@/components/ui/sidebar";
+import { Menu, Search } from "lucide-react";
+import Link from "next/link";
 import * as React from "react";
 import NavMenu from "./nav-menu";
-
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar {...props}>
+    <Sidebar {...props} className="!border-r-0">
       {/* Header */}
-      <SidebarHeader className="h-16 flex items-center justify-center border-b"></SidebarHeader>
+      <SidebarHeader className="flex-row items-center justify-between p-8 border-b">
+        <Link href="/dashboard">
+          <picture>
+            <img src="/images/logo.svg" alt="master POS" className="h-14" />
+          </picture>
+        </Link>
+        <Button size="icon" variant="ghost">
+          <Menu className="size-7" />
+        </Button>
+      </SidebarHeader>
       {/* Content */}
-      <SidebarContent>
+      <SidebarContent className="p-8 gap-8">
+        <div className="relative">
+          <Input placeholder="Search here" className="pl-11" />
+          <Search className="absolute left-4 top-1/2 size-4 -translate-y-1/2 text-muted-foreground" />
+        </div>
         <NavMenu />
       </SidebarContent>
       {/* Rail */}
--- src/app/dashboard/components/nav-menu.tsx
+++ src/app/dashboard/components/nav-menu.tsx
@@ -1,5 +1,12 @@
 "use client";
 
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
 import {
   SidebarGroup,
   SidebarGroupAction,
@@ -8,16 +15,21 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubItem,
 } from "@/components/ui/sidebar";
 
 import {
   ArchiveIcon,
+  ChartLineIcon,
+  ChevronDownIcon,
   Grid2x2Icon,
   Home,
   PlusCircleIcon,
   TagIcon,
 } from "lucide-react";
-import { usePathname } from "next/navigation";
+import Link from "next/link";
+import React from "react";
 
 interface MenuItem {
   label?: string;
@@ -27,6 +39,7 @@ interface MenuItem {
     icon: React.ElementType;
     hasSubMenu?: boolean;
     href?: string;
+    badge?: string;
     subMenu?: {
       label: string;
       icon: React.ElementType;
@@ -36,8 +49,6 @@ interface MenuItem {
 }
 
 export default function NavMenu() {
-  const pathname = usePathname();
-
   const navMain: MenuItem[] = [
     {
       label: "MAIN MENU",
@@ -51,6 +62,7 @@ export default function NavMenu() {
           label: "Products",
           hasSubMenu: true,
           icon: ArchiveIcon,
+          href: "/products",
           subMenu: [
             {
               label: "All Products",
@@ -76,6 +88,17 @@ export default function NavMenu() {
         },
       ],
     },
+    {
+      label: "ANALYTICS",
+      items: [
+        {
+          label: "Sales",
+          href: "/analytics/sales",
+          icon: ChartLineIcon,
+          badge: "49",
+        },
+      ],
+    },
   ];
 
   return navMain.map((item) => (
@@ -86,13 +109,57 @@ export default function NavMenu() {
       )}
       <SidebarGroupContent>
         <SidebarMenu>
-          {item.items.map((item) => (
-            <SidebarMenuItem key={item.label}>
-              <SidebarMenuButton isActive={pathname === item.href}>
-                <item.icon />
-                {item.label}
-              </SidebarMenuButton>
-            </SidebarMenuItem>
+          {item.items.map((item, index) => (
+            <React.Fragment key={index}>
+              {item.hasSubMenu ? (
+                <Collapsible defaultOpen className="group/collapsible">
+                  <SidebarMenuItem>
+                    <CollapsibleTrigger asChild>
+                      <SidebarMenuButton>
+                        <item.icon />
+                        <p className="flex-1">{item.label}</p>
+                        <ChevronDownIcon className=" size-4" />
+                      </SidebarMenuButton>
+                    </CollapsibleTrigger>
+                    <CollapsibleContent>
+                      <SidebarMenuSub>
+                        {item.subMenu?.map((subItem, subIndex) => (
+                          <SidebarMenuSubItem key={subIndex}>
+                            <Button
+                              className="w-full shadow-none justify-start bg-transparent text-muted-foreground hover:bg-transparent hover:text-accent-foreground"
+                              size="sidebar-submenu"
+                              asChild
+                            >
+                              <Link href={subItem.href}>
+                                <subItem.icon />
+                                <p className="flex-1">{subItem.label}</p>
+                              </Link>
+                            </Button>
+                          </SidebarMenuSubItem>
+                        ))}
+                      </SidebarMenuSub>
+                    </CollapsibleContent>
+                  </SidebarMenuItem>
+                </Collapsible>
+              ) : (
+                <SidebarMenuItem key={item.label}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.href!}>
+                      <item.icon />
+                      <p className="flex-1">{item.label}</p>
+                      {item.badge && (
+                        <Badge
+                          variant="accent"
+                          className="size-6 rounded-full font-semibold"
+                        >
+                          {item.badge}
+                        </Badge>
+                      )}
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              )}
+            </React.Fragment>
           ))}
         </SidebarMenu>
       </SidebarGroupContent>
